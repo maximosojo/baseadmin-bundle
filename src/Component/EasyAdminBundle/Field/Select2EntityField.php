bf2c0ebce5c761dd3230af33fbc89a9d2d882845
Máximo Sojo <maxsojo13@gmail.com>
2024-09-06 08:30:28 -0400

    refactor(Select2Entity): Se agrega base para uso en easyadmin.

---
+++ src/Component/EasyAdminBundle/Field/Select2EntityField.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Maximosojo\Bundle\BaseAdminBundle\Component\EasyAdminBundle\Field;
+
+use EasyCorp\Bundle\EasyAdminBundle\Contracts\Field\FieldInterface;
+use Symfony\Contracts\Translation\TranslatableInterface;
+use EasyCorp\Bundle\EasyAdminBundle\Field\FieldTrait;
+use Tetranz\Select2EntityBundle\Form\Type\Select2EntityType;
+
+/**
+ * @author Máximo Sojo <maxsojo13@gmail.com>
+ */
+final class Select2EntityField implements FieldInterface
+{
+    use FieldTrait;
+
+    /**
+     * @param TranslatableInterface|string|false|null $label
+     */
+    public static function new(string $propertyName, $label = null): self
+    {
+        return (new self())
+            ->setProperty($propertyName)
+            ->setLabel($label)
+            ->setTemplateName('crud/field/array')
+            ->setFormType(Select2EntityType::class)
+            ->addCssClass('field-select-2-entity')
+            ->setDefaultColumns('col-md-7 col-xxl-6');
+    }
+}
